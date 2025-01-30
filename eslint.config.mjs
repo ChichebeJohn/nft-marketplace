9b03e64207a9f103cddbc5c5e0bf23a15d5eee68
Chichebe-John <chichebenmesoma@gmail.com>
2025-01-30 04:52:45 +0100

    finaltest for deploy

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,14 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals"),
+  {
+    parser: "@babel/eslint-parser", // Explicitly set the parser
+    parserOptions: {
+      requireConfigFile: false, // Disable the need for Babel config file
+    },
+  },
+];
 
 export default eslintConfig;
